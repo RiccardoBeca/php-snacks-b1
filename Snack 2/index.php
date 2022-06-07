6bbe0ef1d39d08db7fa9e4fbed24e856211a923c
RiccardoBeca <riccardobeca@gmail.com>
2022-06-07 15:28:30 +0100

    snack 2 fixed

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -6,46 +6,48 @@
  -->
 
  <?php 
- 
+// flag
  $isAuthorised = true;
- $errore = '';
- 
- if(empty($_GET['nome']) or empty($_GET['mail']) or empty($_GET['age'])) {
-   
-   $isAuthorised = false;
-   $errore = 'Perfavore inserisci tutti i dati richiesti';
-   
-  } elseif (strlen($_GET['nome']) < 3 ) {
-    
-    $isAuthorised = false;
-    $errore = 'Perfavore inserisci almeno 3 lettere nel tuo nome';
-    
-  } elseif (!strpos($_GET['mail'], '@') or !strpos($_GET['mail'], '.')) {
-    
-    $isAuthorised = false;
-    $errore = "La mail deve contenere '@'e '.'";
-    
-  } elseif (!is_numeric($_GET['age'])) {
-    
-    $isAuthorised = false;
-    $errore = 'Perfavore inserisci un numero';
-    
-  }
-  
-  var_dump($_GET);
 
+ $errore = '';
+
+
+//  se le chiamate get sono vuote, flag diventa falso e ricevo errore
+if(empty($_GET['nome']) or empty($_GET['mail']) or empty($_GET['age'])) {
+   
+  $isAuthorised = false;
+  $errore = 'Perfavore inserisci tutti i dati richiesti';
+} elseif (strlen($_GET['nome']) < 3 ) {
+  //  la lunghezza della stringa nome deve essere lunga almeno 3 char
+    
+  $isAuthorised = false;
+  $errore = 'Perfavore inserisci almeno 3 lettere nel tuo nome';
+} elseif (!strpos($_GET['mail'], '@') or !strpos($_GET['mail'], '.')) {
+  // con strpos trovo la posizione di un qualcosa all'interno di una stringa/ in questo caso con ! se non e' presente il char richiesto mi dara' errore
+    
+  $isAuthorised = false;
+  $errore = "La mail deve contenere '@'e '.'";
+    
+} elseif (!is_numeric($_GET['age'])) {
+    // !is_numeric dichiara se la variabile  e' un numero o una stringa numerica
+  $isAuthorised = false;
+  $errore = 'Perfavore inserisci un numero';
+    
+}
+  
+var_dump($_GET);
+// se la flag riuslta true sono autorizzato
 if($isAuthorised){
 
   $output = 'Ok, sei autorizzato!';
-
+// altrimenti access denied
 } else {
 
   $output = 'Mi dispiace non sei autorizzato!';
 
 }
  
- 
- ?>
+?>
 
 
 <!DOCTYPE html>
@@ -58,7 +60,7 @@ if($isAuthorised){
 </head>
 <body>
   <h1>MAIL CONTROL</h1>
-  
+
   <h2><?php echo $output ?></h2>
 
   <?php if(!$isAuth): ?>
